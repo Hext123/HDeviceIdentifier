7fa101b445ba50728bcfc7c530f24188b10613ee
ios.dev.003 <1411671053@qq.com>
2016-04-05 17:06:40 +0800

    Initial Commit

---
+++ HDeviceIdentifier/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDeviceIdentifier
+//
+//  Created by hext on 16/4/5.
+//  Copyright © 2016年 hext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HDeviceIdentifier/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HDeviceIdentifier
+//
+//  Created by hext on 16/4/5.
+//  Copyright © 2016年 hext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
